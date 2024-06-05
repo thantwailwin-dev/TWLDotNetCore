8ba276bf3b4cbd7d6c88a1c39244deff295c821f
ThantWaiLwin <hcu.thantwailwin@gmail.com>
2024-06-05 10:26:27 +0630

    Layered (N-Layer) Architecture

---
+++ TWLDotNetCore.RestApiWithNLayer/Db/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿/*using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWLDotNetCore.RestApi.Models;
+using TWLDotNetCore.RestApiWithNLayer;*/
+
+namespace TWLDotNetCore.RestApiWithNLayer.Db
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+
+    }
+}
---
+++ TWLDotNetCore.RestApiWithNLayer/GlobalUsings.cs
@@ -0,0 +1,20 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using TWLDotNetCore.RestApi.Models;
+global using TWLDotNetCore.RestApiWithNLayer;
+ 
+
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TWLDotNetCore.RestApiWithNLayer
+{
+    public class GlobalUsings
+    {
+    }
+}
