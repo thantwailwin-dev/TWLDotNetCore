14273a67701dc1184dbd2d7307c538df2023c708
ThantWaiLwin <hcu.thantwailwin@gmail.com>
2024-06-12 15:42:05 +0630

    [Complete] Pizza API

---
+++ TWLDotNetCore.PizzaApi/Db/AppDbContext.cs
@@ -0,0 +1,105 @@
+﻿/*using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWLDotNetCore.RestApi.Models;
+using TWLDotNetCore.RestApiWithNLayer;*/
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TWLDotNetCore.PizzaApi.Db
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<PizzaModel> Pizzas { get; set; }
+        public DbSet<PizzaExtraModel> PizzaExtras { get; set; }
+        public DbSet<PizzaOrderModel> PizzaOrders { get; set; }
+        public DbSet<PizzaOrderDetailModel> PizzaOrderDetails { get; set; }
+    }
+
+    [Table("Tbl_Pizza")]
+    public class PizzaModel 
+    {
+        [Key]
+        [Column("PizzaId")]
+        public int Id { get; set; }
+
+        [Column("Pizza")]
+        public string Name { get; set; }
+
+        [Column("Price")]
+        public decimal Price { get; set; }
+    }
+
+    [Table("Tbl_PizzaExtra")]
+    public class PizzaExtraModel
+    {
+        [Key]
+        [Column("PizzaExtraId")]
+        public int Id { get; set; }
+
+        [Column("PizzaExtraName")]
+        public string Name { get; set; }
+
+        [Column("Price")]
+        public decimal Price { get; set; }
+
+        [NotMapped]
+        public string PriceStr { get { return "$ " + Price; } }
+    }
+
+    public class OrderRequest
+    {
+        public int PizzaId { get; set; }
+        public int[] Extras { get; set; }
+
+    }
+
+    public class OrderResponse
+    {
+        public string meesage {  get; set; }
+        public string invoiceNo { get; set; }        
+        public decimal totalAmount { get; set; }
+
+    }
+
+    [Table("Tbl_PizzaOrder")]
+    public class PizzaOrderModel
+    {
+        [Key]
+        [Column("PizzaOrderId")]
+        public int PizzaOrderId { get; set; }
+
+        [Column("PizzaOrderInvoiceNo")]
+        public string PizzaOrderInvoiceNo { get; set; }
+
+        [Column("PizzaId")]
+        public int PizzaId { get; set; }
+
+        [Column("TotalAmount")]
+        public decimal TotalAmount { get; set; }
+    }
+
+    [Table("Tbl_PizzaOrderDetail")]
+    public class PizzaOrderDetailModel
+    {
+        [Key]
+        [Column("PizzaOrderDetailId")]
+        public int PizzaOrderDetailId { get; set; }
+
+        [Column("PizzaOrderInvoiceNo")]
+        public string PizzaOrderInvoiceNo { get; set; }
+
+        [Column("PizzaExtraId")]
+        public int PizzaExtraId { get; set; }        
+    }
+
+}
---
+++ TWLDotNetCore.PizzaApi/Features/Pizza/PizzaController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TWLDotNetCore.PizzaApi.Db;
+
+namespace TWLDotNetCore.PizzaApi.Features.Pizza
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PizzaController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var lst = await _appDbContext.Pizzas.ToListAsync();
+            return Ok(lst);
+        }
+
+        [HttpGet("Extra")]
+        public async Task<IActionResult> GetExtraAsync()
+        {
+            var lst = await _appDbContext.PizzaExtras.ToListAsync();
+            return Ok(lst);
+        }
+
+        /*[HttpPost("Order")]
+        public async Task<IActionResult> OrderAsync(OrderRequest orderRequest)
+        {
+            var itemPizza = await _appDbContext.Pizzas.FirstOrDefaultAsync(x => x.Id == orderRequest.PizzaId);
+            var total = itemPizza.Price;
+
+            if (orderRequest.Extras.Length > 0)
+            {
+                // select * from Tbl_PizzaExtras where PizzaExtraId in (1,2,3,4)
+                //foreach (var item in orderRequest.Extras)
+                //{
+                //}
+                var lstExtra = await _appDbContext.PizzaExtras.Where(x => orderRequest.Extras.Contains(x.Id)).ToListAsync();
+                total += lstExtra.Sum(x => x.Price);
+            }
+            var invoiceNo = DateTime.Now.ToString("yyyyMMddHHmmss");
+            PizzaOrderModel pizzaOrderModel = new PizzaOrderModel()
+            {
+                PizzaId = orderRequest.PizzaId,
+                PizzaOrderInvoiceNo = invoiceNo,
+                TotalAmount = total
+            };
+            List<PizzaOrderDetailModel> pizzaExtraModels = orderRequest.Extras.Select(extraId => new PizzaOrderDetailModel
+            {
+                PizzaExtraId = extraId,
+                PizzaOrderInvoiceNo = invoiceNo,
+            }).ToList();
+
+            await _appDbContext.PizzaOrders.AddAsync(pizzaOrderModel);
+            await _appDbContext.PizzaOrderDetails.AddRangeAsync(pizzaExtraModels);
+            await _appDbContext.SaveChangesAsync();
+
+            OrderResponse response = new OrderResponse()
+            {
+                invoiceNo = invoiceNo,
+                meesage = "Thank you for your order!Enjoy your pizza!",
+                totalAmount = total,
+            };
+
+            return Ok(response);
+        }*/
+
+        [HttpGet("Order")]
+        public async Task<IActionResult> OrderAsync(OrderRequest orderRequest)
+        {
+            var itemPizza = await _appDbContext.Pizzas.FirstOrDefaultAsync(x => x.Id == orderRequest.PizzaId);
+
+            var total = itemPizza.Price;
+
+            if (orderRequest.Extras.Length > 0)
+            {
+                //*select * from Tbl_PizzaExtra where PizzaExtraId in(1, 2, 3);*//*
+                var extraLst = await _appDbContext.PizzaExtras.Where(x => orderRequest.Extras.Contains(x.Id)).ToListAsync();
+
+                total += extraLst.Sum(x => x.Price);
+            }
+
+            var invoiceNo = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+            PizzaOrderModel pizzaOrderModel = new PizzaOrderModel()
+            {
+                PizzaId = orderRequest.PizzaId,
+                PizzaOrderInvoiceNo = invoiceNo,
+                TotalAmount = total
+            };
+
+            //* List<PizzaOrderDetailModel> pizzaExtraModels = orderRequest.Extras.Select(extraId => new PizzaOrderDetailModel*//*
+            List<PizzaOrderDetailModel> pizzaOrderDetailModel = orderRequest.Extras.Select(extraId => new PizzaOrderDetailModel
+            {
+                PizzaExtraId = extraId,
+                PizzaOrderInvoiceNo=invoiceNo,
+            }).ToList();
+
+            await _appDbContext.PizzaOrders.AddAsync(pizzaOrderModel);
+            await _appDbContext.PizzaOrderDetails.AddRangeAsync(pizzaOrderDetailModel);
+            await _appDbContext.SaveChangesAsync();
+
+            OrderResponse response = new OrderResponse()
+            { 
+                invoiceNo = invoiceNo,
+                meesage = "Thank you for your order!Enjoy your pizza!",
+                totalAmount = total
+            };
+            return Ok(response);
+        }
+    }
+}
